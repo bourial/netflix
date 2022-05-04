8eb06fa6648915f6c38b72f5897fa12609724a46
abdo <abderrazakbourial0@gmail.com>
2022-05-04 00:32:11 +0000

    a

---
+++ components/Header.tsx
@@ -0,0 +1,53 @@
+import Link from "next/link"
+import { useEffect, useState } from "react"
+
+const Header = () => {
+  const [isScrolled, setIsScrolled] = useState<boolean>(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true)
+      } else {
+        setIsScrolled(false)
+      }
+      window.addEventListener("scroll", handleScroll)
+      return () => window.removeEventListener("scroll", handleScroll)
+    }
+  }, [])
+
+  return (
+    <header className={`${isScrolled && 'bg-[#141414]'}`}>
+      {/* left */}
+      <div className="flex items-center space-x-2 md:space-x-10">
+        <img src="https://rb.gy/ulxxee" width={100} height={100} className='cursor-pointer object-contain' alt="logo" />
+        <ul className="hidden space-x-4 md:flex">
+          <li className="header_link">Home</li>
+          <li className="header_link">TV Shows</li>
+          <li className="header_link">Movies</li>
+          <li className="header_link">New & Popular</li>
+          <li className="header_link">My List</li>
+        </ul>
+      </div>
+
+      {/* right */}
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <svg className="hidden sm:inline" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M11.5 21.75C5.85 21.75 1.25 17.15 1.25 11.5C1.25 5.85 5.85 1.25 11.5 1.25C17.15 1.25 21.75 5.85 21.75 11.5C21.75 17.15 17.15 21.75 11.5 21.75ZM11.5 2.75C6.67 2.75 2.75 6.68 2.75 11.5C2.75 16.32 6.67 20.25 11.5 20.25C16.33 20.25 20.25 16.32 20.25 11.5C20.25 6.68 16.33 2.75 11.5 2.75Z" fill="white"/>
+          <path d="M22.0004 22.7499C21.8104 22.7499 21.6204 22.6799 21.4704 22.5299L19.4704 20.5299C19.1804 20.2399 19.1804 19.7599 19.4704 19.4699C19.7604 19.1799 20.2404 19.1799 20.5304 19.4699L22.5304 21.4699C22.8204 21.7599 22.8204 22.2399 22.5304 22.5299C22.3804 22.6799 22.1904 22.7499 22.0004 22.7499Z" fill="white"/>
+        </svg>
+        <p className="hidden lg:inline">Kids</p>
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M12.02 2.90991C8.70997 2.90991 6.01997 5.59991 6.01997 8.90991V11.7999C6.01997 12.4099 5.75997 13.3399 5.44997 13.8599L4.29997 15.7699C3.58997 16.9499 4.07997 18.2599 5.37997 18.6999C9.68997 20.1399 14.34 20.1399 18.65 18.6999C19.86 18.2999 20.39 16.8699 19.73 15.7699L18.58 13.8599C18.28 13.3399 18.02 12.4099 18.02 11.7999V8.90991C18.02 5.60991 15.32 2.90991 12.02 2.90991Z" stroke="white" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round"/>
+          <path d="M13.87 3.19994C13.56 3.10994 13.24 3.03994 12.91 2.99994C11.95 2.87994 11.03 2.94994 10.17 3.19994C10.46 2.45994 11.18 1.93994 12.02 1.93994C12.86 1.93994 13.58 2.45994 13.87 3.19994Z" stroke="white" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M15.02 19.0601C15.02 20.7101 13.67 22.0601 12.02 22.0601C11.2 22.0601 10.44 21.7201 9.90002 21.1801C9.36002 20.6401 9.02002 19.8801 9.02002 19.0601" stroke="white" stroke-width="1.5" stroke-miterlimit="10"/>
+        </svg>
+        <Link href='/acount'>
+          <img src="https://rb.gy/g1pwyx" alt="acount_image" className="cursor-pointer rounded" />
+        </Link>
+      </div>
+    </header>
+  )
+}
+
+export default Header
